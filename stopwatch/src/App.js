5c2a0e018ec161b9cdaa7c16db58069a4531b871
vasu081992 <vasudevan29.92@gmail.com>
2024-05-10 16:34:55 +0530

    stopwatch final

---
+++ stopwatch/src/App.js
@@ -0,0 +1,13 @@
+import Stopwatch from './components/Stopwatch';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Stopwatch/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ stopwatch/src/components/Stopwatch.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect } from "react"
+import {useState} from "react"
+
+
+const Stopwatch = () =>{
+
+const [time,settime] = useState(0)
+// const[timerid,settimerid]=useState()
+
+const [flag,setflag] = useState(false)
+
+const timerOn = ()=>{
+    setflag((prev)=>(!prev))
+}
+
+const timerOff=()=>{
+    settime(0)
+    setflag(false)
+}
+
+const formatTime = (time)=>{
+
+    let minutes = Math.floor(time/60);
+    let remainingSeconds = time%60;
+    //let precisedNum = remainingSeconds.toPrecision(2)
+    return `${minutes} : ${remainingSeconds<10? "0":""}${remainingSeconds}`
+}
+
+useEffect(()=>{
+    console.log("flag state now",flag)
+    let timer;
+    if(flag){
+        timer = setInterval(()=>{
+             settime((prev)=>prev+1)
+         },100)
+         console.log(timer)
+     }
+// else{
+//     console.log("timer cleared")
+//     clearInterval(timer)
+// }
+
+return ()=> {
+    console.log(timer)
+    clearInterval(timer)
+}
+
+},[flag])
+
+    return (
+        <>
+        <h1>StopWatch</h1>
+        <div>Time : {formatTime(time)}</div>
+        <button onClick={timerOn}>{ flag ? "Stop" : "Start" } </button>
+        <button onClick={timerOff}>Reset</button>
+       
+        </>
+    )
+}
+
+export default Stopwatch
